4a5635c88efc4b30374bd7094051dd40478156c2
Stratos Paraskevaidis <stratos1986@gmail.com>
2018-06-04 00:06:12 +0200

    Center text on Contact page

--- client/src/contact.js
+++ client/src/contact.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 function Contact() {
     return (
-        <div>
+        <div id="contactContainer">
             <p>
                 Game collection project created by Stratos Paraskevaidis. For more info, email me at <a href="mailto:dipar59@hotmail.com">dipar59@hotmail.com</a>. 
             </p>
