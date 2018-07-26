1fee69c5f8621a6b644e2d546319e506ca40ea59
Stratos Paraskevaidis <stratos1986@gmail.com>
2018-07-27 00:25:52 +0200

    Change theme

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#ff9e80'
+        }
+    }
+});
+function MyApp() {
+    return (
+        <MuiThemeProvider theme={theme}>
+            <App />
+        </MuiThemeProvider>
+    );
+}
+
+ReactDOM.render(<MyApp />, document.getElementById('root'));
 registerServiceWorker();
